046ecc01aea8e7b6c2a3849d717c2fa3c4e6fc2a
Abeeb ADESINA Adeola <deolabeeb@gmail.com>
2023-10-06 17:30:49 +0000

    Now working with app1.py

--- app1.py
+++ app1.py
@@ -1,39 +1,33 @@
 import chainlit as cl
-
 import os
-huggingfacehub_api_token = os.environ['HUGGINGFACEHUB_API_TOKEN']
 
 from langchain import HuggingFaceHub, PromptTemplate, LLMChain
-# OpenAI
 
-# openai_api_key="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"
-
-# OPENAI_API_KEY=openai_api_key
-
+# Set up your Hugging Face Hub repository and API token
+huggingfacehub_api_token = os.environ['HUGGINGFACEHUB_API_TOKEN']
 repo_id = "tiiuae/falcon-7b-instruct"
-llm = HuggingFaceHub(huggingfacehub_api_token=huggingfacehub_api_token,
-                     repo_id=repo_id, 
-                     model_kwargs={"temperature":0.6, "max_new_tokens":2000})
 
-
+# Create your HuggingFaceHub instance
+llm = HuggingFaceHub(
+    huggingfacehub_api_token=huggingfacehub_api_token,
+    repo_id=repo_id,
+    model_kwargs={"temperature": 0.6, "max_new_tokens": 2000}
+)
 
 template = """
 You are an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.
 
 {question}
-
 """
-
 
 @cl.on_chat_start
 def main():
     # Instantiate the chain for that user session
     prompt = PromptTemplate(template=template, input_variables=["question"])
-    llm_chain = LLMChain(prompt=prompt, llm=OpenAI(temperature=0), verbose=True)
+    llm_chain = LLMChain(prompt=prompt, llm=llm, verbose=True)
 
     # Store the chain in the user session
     cl.user_session.set("llm_chain", llm_chain)
-
 
 @cl.on_message
 async def main(message: str):
@@ -43,7 +37,7 @@ async def main(message: str):
     # Call the chain asynchronously
     res = await llm_chain.acall(message, callbacks=[cl.AsyncLangchainCallbackHandler()])
 
-    # Do any post processing here
+    # Do any post-processing here
 
     # "res" is a Dict. For this chain, we get the response by reading the "text" key.
     # This varies from chain to chain, you should check which key to read.
